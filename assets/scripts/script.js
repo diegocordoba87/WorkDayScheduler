57a7995d060546a8d3caa5e1a32350e80aab48a6
Diego Cordoba <diegocordoba87@hotmail.com>
2020-06-15 14:45:11 -0400

    added IDs to times to compare with moment.js

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,23 +5,41 @@ $(document).ready(function () {
     $("#currentDay").text(present.format("dddd, MMMM, Do"));
     }
     presentTime();
-    var times = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM","5PM"];
-    
-    function timeSlot (){
-        for (i=0 ; i <= times.length -1; i++){   
-               
-            $(".container").append("<div class= 'row'>");
-            
-        }
+
+for(i =0; i < 9;i++){
+    var storedItem = localStorage.getItem(`event${i}`);
+    if(storedItem){
+    console.log(storedItem);
+    console.log(`input${i}`)
+    var id = $(`#input${i}`)
+    console.log(id);
+    $(id).val(storedItem);
     }
-    timeSlot();
     
-    $(".row").append("<div class= 'col-1 time-block hour'>");
-    $(".row").append("<input class= 'col-10 eventInput textarea''>");
-    $(".row").append("<div class= 'col-1 display-icon saveBtn btn fas fa-lock'>");  
+
+}
+
+    //select all buttons under class
+$(".saveBtn").on("click", function(){
+    console.log("clicking");
+
+    // we want to get the ID for the button. "this" refers to the button, and we are asking for its ID as we click.
+    var buttonId = ($(this).attr("id"));
+    console.log(buttonId);
+
+    var event = `#input${buttonId}`;
+    console.log(event);
+    console.log($(event).val());
+    localStorage.setItem(`event${buttonId}`,$(event).val());
+    console.log(`event${buttonId}`,$(event).val());
+})
+
+
+
+
+
     
    
-    
 //moment("12:00").isSame("12:01")
 //moment("12:00").isBefore("12:01")
 
