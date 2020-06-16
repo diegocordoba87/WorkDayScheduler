6c3cc0ce2446e006b5bb534cf4b679735f7c3d31
Diego Cordoba <diegocordoba87@hotmail.com>
2020-06-15 21:10:57 -0400

    added color code per time

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -9,30 +9,26 @@ $(document).ready(function () {
   for (i = 0; i < 9; i++) {
     var storedItem = localStorage.getItem(`event${i}`);
     if (storedItem) {
-      console.log(storedItem);
-      console.log(`input${i}`);
       var id = $(`#input${i}`);
-      console.log(id);
+      
       $(id).val(storedItem);
     }
   }
 
   function timeColor() {
+    
     for (i = 0; i < 9; i++) {
         var hour = $(".hour");
-        console.log($(".row"));
-        var hourId = $(hour[i]).attr("id");
+        var hourId = $(hour[i]).attr("id"); 
         var currentHour = present.format("k");
-        console.log(hourId);
-        console.log(currentHour);
-      if (hourId < currentHour) {
-        $("input").addClass("past");
+      if (hourId  < currentHour) {
+        $("#input"+i).addClass("past");
       }
-      else if (hourId === currentHour) {
-        $("input").addClass("present");
+      else if (hourId  == currentHour) {
+        $("#input"+i).addClass("present");
       }
-      else if (hourId < currentHour) {
-        $("input").addClass("future");
+      else if (hourId > currentHour) {
+        $("#input"+i).addClass("future");
       }
     }   
   }
@@ -46,13 +42,9 @@ $(document).ready(function () {
 
     // we want to get the ID for the button. "this" refers to the button, and we are asking for its ID as we click.
     var buttonId = $(this).attr("id");
-    console.log(buttonId);
-
     var event = `#input${buttonId}`;
-    console.log(event);
-    console.log($(event).val());
     localStorage.setItem(`event${buttonId}`, $(event).val());
-    console.log(`event${buttonId}`, $(event).val());
+
   });
 
 });
